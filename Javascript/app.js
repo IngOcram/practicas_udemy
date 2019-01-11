a69cb605703fcc63f5da66cd9f5e6e740f8533d7
Marco Flores <marco.flores85@hotmail.com>
2019-01-10 20:05:04 -0600

    var, let const

--- Javascript/app.js
+++ Javascript/app.js
@@ -5,4 +5,25 @@ document.getElementById('app').innerHTML =  `Bienvenido ${nombre} de ${edad} añ
 
 console.log("enviando a la consola");
 
+var carrito = "libro", carrito1 = "libro2";
+
+console.log(carrito);
+
+var primerNombre = 'Mark'; //vamelcase
+var primer_nombre = 'Mark'; //underscore
+var Primernombre = 'Mark'; //pascal case
+var totalcarrito 
+
+console.log(nombre);
+
+let producto1 = 'libro';
+
+console.log(producto1);
+
+const elemento = 'agua';
+
+console.log(elemento);
+
+
+
 
---
+++ Javascript/app2.js
@@ -0,0 +1,18 @@
+let nombre, mensaje;
+
+nombre = 'Marco A';
+nombre = "Marco A";
+// nombre = 'Marco A";
+mensaje = 'y entonces dije "buen curso!!"';
+mensaje = 20;
+
+// concatenar javascript
+
+mensaje = 'java' + 'javascript';
+
+// uniendo variables en una sola
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'javascript';
+
+console.log(`${Aprendiendo} ${tecnologia}`);
