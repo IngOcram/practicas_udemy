c9004f1fce85b9c767e1f05bddf880883a4aaa36
Marco Flores <marco.flores85@hotmail.com>
2019-01-17 22:01:41 -0600

    Numeros en JS

---
+++ Javascript/numero.js
@@ -0,0 +1,47 @@
+// Numero en JS
+
+const numero1 = 30,
+      numero2 = 20,
+      numero3 = 20.20,
+      numero4 = .1020,
+      numero5 = -3;
+
+
+let resultado;
+
+// Suma
+resultado = numero1 + numero2;
+// Resta
+resultado = numero1 - numero2;
+// Multiplicar
+resultado = numero1 * numero2;
+// Divisiones
+resultado = numero1 / numero2;
+// Modulo
+resultado = numero1 % numero2;
+// Pi
+resultado = Math.PI;
+//Redondeo
+resultado = Math.round(2.5);
+resultado = Math.ceil(3.6);
+resultado = Math.floor(4.5);
+// Raiz Cuadrada
+resultado = Math.sqrt(144);
+//Absoluto
+resultado = Math.abs(numero5);
+//Potencia
+resultado = Math.pow(8, 3);
+//Minimo
+resultado = Math.min(3,4,8,10,11);
+//Miximo
+resultado = Math.max(3,4,8,10,11);
+//aleatorio
+resultado = Math.random();
+
+resultado = (10 + 20) * 5;
+
+resultado = (10 + 20 + 10 + 40)*.20;
+
+
+console.log(resultado);
+
