0f1e6ee2881d3bc112572ef42ae53f62252b8086
Marco Flores <marco.flores85@hotmail.com>
2019-01-19 18:26:11 -0600

    tipos de datos, comparacion

---
+++ Javascript/comparacion.js
@@ -0,0 +1,25 @@
+const numero1 = 20;
+const numero2 = 50;
+const numero3 = '20';
+let nombre = 'juan';
+let nombre = 'Marco'; 
+//console.log(numero1 > numero2);
+//console.log(numero1 < numero2);
+
+// comparador de igual
+//console.log( numero1 === numero3 );
+
+//Diferentes
+//console.log(2 != 3)
+
+//console.log('hola' == ' hola');
+
+//console.log('a' > 'b');
+
+//console.log(null == undefined);
+
+console.log( nombre == nombre2);
+
+
+
+
---
+++ Javascript/datos.js
@@ -0,0 +1,30 @@
+let valor;
+
+valor = 'marco';
+valor = 20;
+valor = 20.20;
+valor = -30;
+valor = "20";
+
+//Buleano
+valor = false;
+
+//null
+valor = null;
+
+//symbol
+valor = symbol('simbolo');
+
+//arreglo
+valor = [1,2,3,4];
+
+//objeto
+valor = {
+    nombre: 'Marco',
+    Profesion: 'ingeniero'
+}
+
+//fecha
+valor = new Date();
+
+console.log(typeof valor);
