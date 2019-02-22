10771233eb6de0df0c94f50efa833cc06228afb0
Marco Flores <marco.flores85@hotmail.com>
2019-02-21 23:00:57 -0600

    curso JS UDEMY

---
+++ Javascript/numeros.js
@@ -0,0 +1,46 @@
+const numero1 = "50",
+      numero2 = 10,
+      numero3 = 'tres';
+
+//console.log( numero1 + numero2);
+
+//console.log(Number(numero1) + numero2);
+
+//console.log(parseInt(numero1) + numero2);
+
+//console.log( numero1 - numero2);
+
+//console.log(typeof parseInt(numero1));
+
+let dato;
+
+dato = Number("20");
+dato = Number("20.1254");
+dato = Number(true);
+dato = Number(false);
+dato = Number(null);
+dato = Number(undefined);
+dato = Number([1,2,3]);
+
+//ParseFloat = numeros flotantes y ParseInt = numeros enteros
+
+dato = parseInt('100');
+dato = parseFloat('100');
+dato = parseFloat('100.2030');
+dato = parseInt('100.2030');
+
+// toFixed
+
+dato = '156484213186464321';
+//dato = 156484213186464321.163161116116;
+
+//console.log(dato.toFixed(3));
+//console.log(typeof dato);
+
+console.log
+
+
+
+
+
+
---
+++ Javascript/producto.js
@@ -0,0 +1,32 @@
+//let producto = 'libro';
+
+//producto = 'libro2';
+
+//console.log(producto);
+
+let nombre, mensaje;
+
+nombre = 'Marco Flores'
+nombre = "Marco Flores"
+
+//mensaje = 'Entonces dije "buen curso !!"  '
+
+//console.log(mensaje, nombre);
+
+//concatenar JS
+
+//mensaje = 'java' + 'script';
+
+//console.log(mensaje);
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = 'Javascript';
+
+console.log( `${aprendiendo} ${tecnologia}`);
+
+
+
+
+
+
+
