1bd35e11023c1377195f35c14ea7c4fe40b60773
IngOcram <marco.flores85@hotmail.com>
2019-10-05 00:58:23 -0500

    Calendario JS

--- DevCode/Calendario/Calendario.js
+++ DevCode/Calendario/Calendario.js
@@ -1,3 +1,6 @@
 let mes = document.createElement("DIV");
 mes.className = "mes";
 document.body.appendChild(mes);
+let tabla_mes = document.createElement("TABLE");
+tabla_mes.className = "tabla_mes";
+mes.appendChild(tabla_mes);
